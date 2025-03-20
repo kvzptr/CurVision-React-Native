c809b22571f5fc1ffdb2c5de734c5be9da240234
Kevin Peter L. Adaya <labajokevinpeter@gmail.com>
2025-03-21 03:20:35 +0800

    commit #	renamed:    app/index.jsx -> app/CurVision.jsx commit #	modified:   app/home_page.jsx commit #	new file:   components/BackgroundSVG.jsx commit #	modified:   package-lock.json commit #	modified:   package.json commit #	modified:   tsconfig.json

--- app/home_page.jsx
+++ app/home_page.jsx
@@ -2,25 +2,12 @@ import React, { useEffect, useRef } from 'react';
 import { View, Text, Image, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
 import LottieView from 'lottie-react-native'; 
 import { useNavigation } from '@react-navigation/native'; 
+import { Ionicons } from '@expo/vector-icons';
+import BackgroundSVG from '../components/BackgroundSVG';
 
 const Homepage = () => {
   const navigation = useNavigation();
   const horizontalScrollView = useRef(null);
-  const scrollSpeed = 2;
-
-  // Auto-scroll logic
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (horizontalScrollView.current) {
-        horizontalScrollView.current.scrollTo({
-          x: horizontalScrollView.current.contentOffset.x + scrollSpeed,
-          animated: true,
-        });
-      }
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, []);
 
   const navigateTo = (screen) => {
     navigation.navigate(screen);
@@ -31,40 +18,43 @@ const Homepage = () => {
       <View style={styles.relativeContainer}>
         <Image
           style={styles.logo}
-          source={require('./assets/images/logo.png')}
+          source={require('../assets/images/logo.png')}
         />
         <Text style={styles.welcomeText}>Welcome</Text>
 
-        <ion-icon name="notifications-outline"></ion-icon>
-        
-        <View style={styles.textAndAnimationContainer}>
-          <Text style={styles.exploreText}>Explore more with Curvision</Text>
+        <Ionicons name="notifications-outline" size={30} color="white" marginTop={35}  />
+      </View>
+
+      <View style={styles.textAndAnimationContainer}>
+          <Text style={styles.exploreText}>Explore more with</Text>
+          <Text style={styles.curVisionText}>CurVision</Text>
           <LottieView
-            source={require('./assets/car_animation.json')}
+            source={require('../assets/car_animation.json')}
             autoPlay
             loop
             style={styles.lottieAnimation}
           />
         </View>
-      </View>
+
+      <BackgroundSVG style={styles.backgroundSVG} />
 
       <View style={styles.imageContainer}>
         <TouchableOpacity onPress={() => navigateTo('HistoryPage')}>
           <Image
             style={styles.image}
-            source={require('./assets/images/ha.png')} 
+            source={require('../assets/images/ha.png')} 
           />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigateTo('EmergencyPage')}>
           <Image
             style={styles.image}
-            source={require('./assets/images/ec.png')} 
+            source={require('../assets/images/ec.png')} 
           />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigateTo('VehiclesPage')}>
           <Image
             style={styles.image}
-            source={require('./assets/images/va.png')} 
+            source={require('../assets/images/va.png')} 
           />
         </TouchableOpacity>
       </View>
@@ -80,19 +70,19 @@ const Homepage = () => {
         >
           <Image
             style={styles.newsImage}
-            source={require('./assets/images/n1.png')} 
+            source={require('../assets/images/n1.png')} 
           />
           <Image
             style={styles.newsImage}
-            source={require('./assets/images/n2.png')} 
+            source={require('../assets/images/n2.png')} 
           />
           <Image
             style={styles.newsImage}
-            source={require('./assets/images/n3.png')} 
+            source={require('../assets/images/n3.png')} 
           />
           <Image
             style={styles.newsImage}
-            source={require('./assets/images/n4.png')} 
+            source={require('../assets/images/n4.png')} 
           />
         </ScrollView>
       </View>
@@ -102,45 +92,68 @@ const Homepage = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, 
+    backgroundColor: 'transparent',
+    position: 'relative',
+  },
+  backgroundSVG: {
+    position: 'absolute', // Position the SVG absolutely
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
   },
   relativeContainer: {
-    height: 266,
-    backgroundColor: '#ececec', // Replace with actual background color
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    paddingTop: 0,
+    height: 100,
+    backgroundColor: '#800000',
     position: 'relative',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   logo: {
     width: 50,
     height: 50,
-    marginTop: 20,
+    marginTop: 35,
   },
   welcomeText: {
     marginTop: 35,
     fontSize: 24,
     color: 'white',
+    marginRight: 20,
   },
   textAndAnimationContainer: {
-    marginTop: 80,
+    paddingTop: 20,
+    backgroundColor: '#800000',
+    marginTop: 0,
+    marginBottom: 0,
     alignItems: 'center',
   },
   exploreText: {
     fontSize: 28,
     color: 'white',
   },
+  curVisionText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: 'gold',
+  },
   lottieAnimation: {
     width: 300,
     height: 300,
-    marginTop: -70,
+    marginTop: -40,
   },
   imageContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
-    backgroundColor: '#ececec', // Replace with actual background color
+    background: 'transparent', 
     height: 200,
     alignItems: 'center',
+    marginTop: -350,
   },
   image: {
     width: 120,
@@ -149,38 +162,31 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   newsSection: {
-    padding: 20,
-    backgroundColor: '#ececec', // Replace with actual background color
+    marginTop: 15,
+    padding: 0,
+    background: 'transparent',
   },
   newsText: {
+    marginLeft: 20,
     fontSize: 25,
     fontWeight: 'bold',
+    top: -10,
   },
   seeAllText: {
     fontSize: 12,
     color: 'black',
     position: 'absolute',
     right: 20,
-    top: 20,
+    top: 3,
   },
   scrollViewContainer: {
     flexDirection: 'row',
+    background: 'transparent',
   },
   newsImage: {
     width: 140,
     height: 180,
     marginRight: 15,
-  },
-  bottomNavigation: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    backgroundColor: '#ececec', // Replace with actual background color
-    paddingVertical: 10,
-  },
-  navIcon: {
-    width: 25,
-    height: 25,
-    marginHorizontal: 30,
   },
 });
 
---
+++ components/BackgroundSVG.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+const BackgroundSVG = () => {
+  return (
+    <Svg
+      width="420"
+      height="250"
+      viewBox="0 0 350 210"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <Path
+        d="M279.2 88.62C180.46 159.5 37.92 59.08-21 0h404.6c6.34 0-5.69 17.72-104.4 88.62Z"
+        fill="#800000"
+      />
+    </Svg>
+  );
+};
+
+export default BackgroundSVG;
